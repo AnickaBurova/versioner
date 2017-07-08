1f24d722f26858ef18f28646ad11e9eb1cb88b8a
Anicka Burova <anicka.burova@gmail.com>
2017-07-08 08:07:33 +0100

    0.2.1.0: Test

--- versioner.py
+++ versioner.py
@@ -44,7 +44,7 @@ import re
 
 
 usage = "usage: %prog [options] file"
-version = "0.2.0.0"
+version = "0.2.1.0"
 version_text = "%prog {}".format(version)
 opt = OptionParser(usage = usage, version = version_text)
 opt.add_option  ("-l","--language"
